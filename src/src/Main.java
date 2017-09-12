8ec6b52b56d93003553dc6368eb151fdff6ce856
14241A05E1 <m.saiprasanna981997@gmail.com>
2017-09-12 12:37:29 +0530

    second

---
+++ src/src/Main.java
@@ -0,0 +1,26 @@
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+
+
+public class Main {
+    public static Date Date1( String s) throws ParseException
+            {
+          
+Date date1=new SimpleDateFormat("dd/MM/yyyy").parse(s);
+            return date1;}
+
+
+	public static void main(String[] args) throws ParseException {
+            Student ob1;
+		
+		//You may test that your code works find here
+		//Please check that your code works and has no 
+		//compilation problems before to submit
+            ob1 = new Student(1,"prasanna",Date1("9/8/1997"),98.9);
+            
+	
+
+}}
